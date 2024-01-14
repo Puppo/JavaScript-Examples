232199f6b64f17a0229fbf01251959c853351380
Luca Del Puppo <luca@delpuppo.net>
2024-01-14 12:43:30 +0100

    update last example

--- src/03-for-await-of-loops.js
+++ src/03-for-await-of-loops.js
@@ -1,8 +1,14 @@
-const delay = (ms, v) => new Promise((resolve) => setTimeout(() => {
+const delay = (ms, v) => new Promise((resolve, reject) => {
+  if (!ms) {
+    return reject(new Error(`ms should be greater than 0 but it's equals to ${ms}`))
+  }
+  setTimeout(() => {
   console.log(`delay ${ms}ms resolved with ${v}`)
   resolve(v)
-}, ms))
+}, ms)
+})
 const delays = [2000, 1000, 3000]
+const delaysWithError = [3000, 1000, 0]
 
 function RangeWithDelay(from, to, ms) {
   this.from = from
@@ -19,14 +25,26 @@ function RangeWithDelay(from, to, ms) {
   return this
 }
 
+let numbers = [1, 2, 3, 4, 5];
+
 async function main() {
+  console.log('\nfor await of range with delay')
+  for await (const item of new RangeWithDelay(1, 5, 1000)) {
+    console.log(item)
+  }
+
+  console.log('\n for await with array')
+  for await (const item of numbers) {
+    console.log(item)
+  }
+
   console.log('for await of delays')
   for await (const item of delays.map(delay)) {
     console.log(item)
   }
 
-  console.log('\nfor await of range with delay')
-  for await (const item of new RangeWithDelay(1, 5, 1000)) {
+  console.log('for await of delaysWithError')
+  for await (const item of delaysWithError.map(delay)) {
     console.log(item)
   }
 }
