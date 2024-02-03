77f4f7dc27695deea55eed2291471feefb56b5ac
Luca Del Puppo <luca@delpuppo.net>
2024-02-03 18:33:16 +0100

    fix: range with delay

--- src/03-for-await-of-loops.js
+++ src/03-for-await-of-loops.js
@@ -13,12 +13,12 @@ const delaysWithError = [3000, 1000, 0]
 function RangeWithDelay(from, to, ms) {
   this.from = from
   this.to = to
-  this.delay = delay
+  this.ms = ms
 
   this[Symbol.asyncIterator] = async function* () {
     let current = this.from
     while (current <= this.to) {
-      yield await delay(ms, current++)
+      yield await delay(this.ms, current++)
     }
   }
 
@@ -28,25 +28,15 @@ function RangeWithDelay(from, to, ms) {
 let numbers = [1, 2, 3, 4, 5];
 
 async function main() {
-  console.log('\nfor await of range with delay')
-  for await (const item of new RangeWithDelay(1, 5, 1000)) {
-    console.log(item)
-  }
-
-  console.log('\n for await with array')
-  for await (const item of numbers) {
-    console.log(item)
-  }
-
-  console.log('for await of delays')
-  for await (const item of delays.map(delay)) {
-    console.log(item)
-  }
-
   console.log('for await of delaysWithError')
-  for await (const item of delaysWithError.map(delay)) {
-    console.log(item)
+  try {
+    for await (const item of delaysWithError.map(delay)) {
+      console.log(item)
+    }
+  } catch (e) {
+    console.error(e)
   }
+
 }
 
 main()
